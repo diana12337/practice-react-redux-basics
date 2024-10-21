7428a3c8b44e824710852b433d2b89b7a71ef3f2
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-10-21 20:50:59 +0200

    add task 01

--- 01/Task01.js
+++ 01/Task01.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Subject from './../src/libs/Subject';
 
 const Task01 = () => {
+    const subject = new Subject();
     const [time, setTime] = useState(0);
     const [tagName, setTagName] = useState('??');
     const [cursorPosition, setCursorPosition] = useState('[?,?]');
@@ -9,23 +10,24 @@ const Task01 = () => {
     const setEventTime = ({timeStamp}) => {
         setTime( parseInt(timeStamp / 1000) );
     }
+    subject.subscribe(setEventTime);
 
     const setEventTagName = ({target}) => {
         setTagName(target.tagName);
     }
+    subject.subscribe(setEventTagName);
 
     const setEventCursorPosition = ({pageX, pageY}) => {
         setCursorPosition(`[${pageX},${pageY}]`);
     }
-
+    subject.subscribe(setEventCursorPosition);
     return (
         <section>
             <h1>Task 1</h1>
             
             <div onClick={ event => {
-                setEventTime(event);
-                setEventTagName(event);
-                setEventCursorPosition(event);
+               subject.notify(event);
+                
             }}>
                 <p>
                     <strong>Kliknij wybrany element:</strong> <a>link</a>, <button>button</button>, <span>span</span>
