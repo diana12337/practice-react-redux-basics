0b4778e410e9f0eadfad11245a9d90cb90a0e8d5
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-10-22 18:10:01 +0200

    add tasks 02, 03

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from'redux';
 
 import Task01 from './../../01/Task01';
 import Task02 from './../../02/Task02';
@@ -8,15 +10,39 @@ import Task05 from './../../05/Task05';
 
 
 const App = () => {
+
+    const initState = { 
+        message: "Działa",
+        time: new Date() 
+    };
+    
+    const reducer = (state = initState, action) => {
+        switch(action.type) {
+            case'getCurrentTime':
+     
+            return  {...state, time:new Date() }
+                      
+        }
+    return state}
+    
+
+    const store = createStore(reducer,
+
+window.__REDUX_DEVTOOLS_EXTENSION__    && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+  
     return (
-        <>
-            <Task01 />
-            {/* <Task02 /> */}
-            {/* <Task03 /> */}
+        <Provider store={store}>
+             <Task01 />
+             <Task02 /> 
+             <Task03 /> 
             {/* <Task04 /> */}
             {/* <Task05 /> */}
-        </>
+            
+       </Provider>
     )
 }
 
-export default App;
+
+export default App
